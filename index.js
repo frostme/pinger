384f3ad3099783a899eaf8515d8266479d6b6ce7
M. Elliot Frost <melliotfrost@gmail.com>
2014-11-22 22:30:11 -0500

    Initial Commit

---
+++ index.js
@@ -0,0 +1,22 @@
+var CronJob = require('cron').CronJob;
+var http    = require('http');
+var stopped = function(){
+	console.log('stopped');
+};
+module.exports = {
+	schedule: function(pattern, cb){
+		var j = new CronJob(pattern, cb, stopped, true);
+	},
+	monitor: function(sites){
+		var self = this;
+		sites.forEach(function(site){
+			self.schedule('00,10,20,30,40,50 * * * * *', function(){
+				http.get(site, function(res){
+					console.log("Got response: " + res.statusCode);
+				}).on('error', function(e){
+					console.log("Got error: " + e.message);	
+				});
+			});
+		});
+	}
+}
