9834bcb8a3a396add1ac1e4ea5ba9e40b87c96ef
M. Elliot Frost <melliotfrost@gmail.com>
2014-11-23 11:55:43 -0500

    Adding the stop function to monitoring

--- test.js
+++ test.js
@@ -1,5 +1,8 @@
-var pinger = require('./index');
+var cronPing = require('./index');
+var fileobj  = require('fileobj');
 var readJSON = require('read-json');
 readJSON('./test.json', function(err, sites){
-	pinger.monitor(sites);
+	cronPing.monitor(sites, function(){
+		
+	});
 });
